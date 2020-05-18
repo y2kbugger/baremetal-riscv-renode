66751b52c18ef28db679a9a6b5546edd977499e7
y2k <git@y2kbugger.com>
2020-05-18 00:25:29 -0400

    Newer

--- irq.s
+++ irq.s
@@ -5,35 +5,83 @@
 .equ EventStatus, 0x0c
 .equ EventPending, 0x10
 .equ EventEnable, 0x14
+# UART masks
+.equ TxEvent, 0b01
+.equ RxEvent, 0b10
 
 .section .text
 .global _start      # Provide program starting address to linker
 
-# Loop forever counting on a0
-_start: addi a0, x0, 1
+_start:
+
 # setup machine trap vector
 1:      auipc   t0, %pcrel_hi(mtvec)        # load mtvec(hi)
         addi    t0, t0, %pcrel_lo(1b)       # load mtvec(lo)
-        csrrw   zero, mtvec, t0
+        #csrrw   zero, mtvec, t0
+# The assembler pseudoinstruction to write a CSR, CSRW csr, rs1, is encoded as CSRRW x0, csr, rs1
+        csrw   mtvec, t0
 
-# set mstatus.MIE=1 (enable M mode interrupt)
-        li      t0, 8
+# set mstatus.MIE=1 (enable M mode interrupts in general)
+        li      t0, 0b00001000
         csrrs   zero, mstatus, t0
+
+# set mie.MTIE=1 (enable M mode timer interrupts)
+        li      t0, 0b0000000010000000
+        csrrs   zero, mie, t0
+
+# set mie.MEIE=1 (enable M mode external interrupts)
+        li      t0, 0b0000100000000000
+        csrrs   zero, mie, t0
+
+# set EnableEvents.rxevents = 1
+        li a2, UART_BASE
+        addi a1, x0, RxEvent
+        sb a1, EventEnable(a2)
+
+# just to show that we got here
+        addi a3, x0, 0xBA
 
 sleep:
         wfi # wait for interrupt
         j sleep
 
+.align 4
 # break on interrupt
 mtvec:
-        j print_and_sleep
+# Acknowledge received char
+        li a2, UART_BASE
+        lb a7, RxTx(a2)
+        #addi a1, x0, 0b11
+        #sb a1, EventPending(a2)
+# Put out some characters
+        csrr t0, mcause
+        li a2, UART_BASE
 
-print_and_sleep:
-        la a0, pass_msg
-        j sleep
+        addi a1, a7, 0
+        sb a1, RxTx(a2)
+        sb a1, RxTx(a2)
+        sb a1, RxTx(a2)
+        addi a1, x0, '\n'
+        sb a1, RxTx(a2)
 
+        addi a1, a7, 0
+        sb a1, RxTx(a2)
+        sb a1, RxTx(a2)
+        sb a1, RxTx(a2)
+        sb a1, RxTx(a2)
+        addi a1, x0, '\n'
+        sb a1, RxTx(a2)
 
-.section .rodata
+        addi a1, a7, 0
+        sb a1, RxTx(a2)
+        sb a1, RxTx(a2)
+        sb a1, RxTx(a2)
+        addi a1, x0, '\n'
+        sb a1, RxTx(a2)
 
-pass_msg:
-        .string "PASS\n"
+        mret
+
+putbyte: # char in a1
+        li a2, UART_BASE
+        sb a1, RxTx(a2)
+        ret
