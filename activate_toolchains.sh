6993adc3f68360448c9ad6a442d94da1211fff6d
y2k <git@y2kbugger.com>
2020-09-03 01:13:06 -0400

    Add guard to make to remind to activate

--- activate_toolchains.sh
+++ activate_toolchains.sh
@@ -7,6 +7,6 @@ fi
 
 # only source once
 if [ -n "$BAREMETAL_RISCV_RENODE" ]; then echo "Already ran"; return; fi
-BAREMETAL_RISCV_RENODE=$(realpath $(dirname "$BASH_SOURCE"))
+export BAREMETAL_RISCV_RENODE=$(realpath $(dirname "$BASH_SOURCE"))
 
 PATH="$BAREMETAL_RISCV_RENODE/renode:$BAREMETAL_RISCV_RENODE/riscv-gnu-toolchain-output/bin:$PATH"
