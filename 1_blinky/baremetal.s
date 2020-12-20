6824e964bdda4ec386d2ba7b5b63baf967cd90b0
y2k <git@y2kbugger.com>
2020-12-20 16:06:32 -0500

    Put the blink-mask in a register

--- 1_blinky/baremetal.s
+++ 1_blinky/baremetal.s
@@ -1,11 +1,14 @@
 .equ LED, 0x60000800
 .equ DELAY_COUNT, 9000000
+.equ LED_STATE_INITIAL, 0b00
+.equ LED_STATE_TOGGLE_MASK, 0b01
 
 .section .text
 .global _start
 _start:
         li a5, LED
-        li a4, 0b01
+        li a4, LED_STATE_INITIAL
+        li a6, LED_STATE_TOGGLE_MASK
         sw a4, 0x0(a5)
 loop:
         li a0, DELAY_COUNT      # reset counter
@@ -14,6 +17,6 @@ delay_loop:
         bnez a0, delay_loop
 toggle_led:
         lw a4, 0x0(a5)          # read in old led state
-        xori a4, a4, 0b11       # toggle led state word
+        xor a4, a4, a6          # toggle led state word
         sw a4, 0x0(a5)          # write new state
         jump loop, t0
