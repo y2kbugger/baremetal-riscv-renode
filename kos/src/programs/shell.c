b4beb3d0aea6cc9a45ed25cf69cd24d4d76c7435
y2k <git@y2kbugger.com>
2021-08-29 08:11:13 -0400

    Make shell synchronous by default, with ability to interrupt & stop

--- kos/src/programs/shell.c
+++ kos/src/programs/shell.c
@@ -1,37 +1,55 @@
 #include "../baremetal.h"
 #include "../uart.h"
+#include "../timer.h"
 #include "programs.h"
 
+void _usage()
+{
+    puts("Programs:\n");
+    puts("    h: hello world\n");
+    puts("    l: laugh\n");
+    puts("    f: laugh forever\n");
+    puts("    c: count forever\n");
+
+    puts("Shell commands:\n");
+    puts("    ?: show this help\n");
+    // puts("    @: list stopped processes\n");
+    // puts("    ^: restart all stopped processes in background\n");
+    puts("    !: stop all processes. This can be used while program\n\tis running in foreground or at prompt to kill any background processes.\n");
+}
 void shell()
 {
     while (1)
     {
-        puts("Programs:\n");
-        puts(" h: hello world\n");
-        puts(" l: laugh\n");
-        puts(" f: laugh forever\n");
-        puts(" c: count forever\n");
-        puts("Launch one or more programs by submitting characters.\n");
-        // puts("! will stop the currently running process.\n");
-        // puts("An empty return will list info about currently running processes.\n");
         puts("kos> ");
 
         char name;
         while ((name = getc()) == '\n')
             ;
+        putc('\n');
 
-        if (name == '!')
+        if (name == '?')
         {
-            // handling stopping a process here
-            // last_process->status = Stopping;
+            _usage();
             continue;
         }
 
-        if (NULL == init_process(lookup_program(name)))
+        struct Process *proc;
+        if (NULL == (proc = init_process(lookup_program(name))))
         {
             puts("Failed to start ");
             putc(name);
             putc('\n');
+        }
+
+        while (proc->status == Ready && peekc() != '!')
+            ;
+
+        if (peekc() == '!')
+        {
+            getc();
+            putc('\n');
+            stop_all_processes_except(current_process);
         }
     }
 }
