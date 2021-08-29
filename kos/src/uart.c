15782e17ba0d6a3d37714b54cd4ced0e4ab73ec4
y2k <git@y2kbugger.com>
2021-08-29 08:08:51 -0400

    Add ability to peek at next character

--- kos/src/uart.c
+++ kos/src/uart.c
@@ -29,9 +29,32 @@ void puts(char *s)
         putc(*s++);
 }
 
+static char _buffer = '\0';
 char getc()
 {
+    if (_buffer != '\0')
+    {
+        char val = _buffer;
+        _buffer = '\0';
+        return val;
+    }
     while (uart->RxEmpty)
         ;
     return uart->RxTx;
+}
+
+// Nonblocking peek of next character
+// If no character ready (buffered or raw), return NULL
+// If character has already been peeked
+//   AND there is a new char
+//   disgard buffered char and peek again.
+char peekc()
+{
+    if (uart->RxEmpty)
+        return _buffer;
+    else
+    {
+        _buffer = uart->RxTx;
+        return _buffer;
+    }
 }
--- kos/src/uart.h
+++ kos/src/uart.h
@@ -1,6 +1,7 @@
 void init_uart();
 
 char getc();
+char peekc();
 
 void putc(char c);
 void puts(char *s);
