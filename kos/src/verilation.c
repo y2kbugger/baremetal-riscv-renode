f5722052c02d126b858176e3f54d4847061e69c7
y2k <git@y2kbugger.com>
2023-08-29 23:10:19 -0400

    Add subsequent multiplies to test case

--- kos/src/verilation.c
+++ kos/src/verilation.c
@@ -42,62 +42,65 @@ int verilation_test()
         {4, 3, 2}};
     print_matrix("B", b_values);
 
-    // Copy matrices to the matmul structure
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            matmul->A[i][j] = a_values[i][j];
-            matmul->B[i][j] = b_values[i][j];
-        }
-    }
-
-    // Start the multiplication
-    // Depending on your architecture, you might need to set an 'Enable' flag.
-    // Here, I'll assume the multiplication starts automatically when A and B are set.
-
-    // After this, the result should be available in matmul->C
-    // And you can further process or verify it as needed.
-    // Example:
     uint8_t expected_c_values[3][3] = {
         {93, 150, 126},
         {57, 96, 81},
         {21, 42, 36}};
     print_matrix("Expected C", expected_c_values);
 
-    uint8_t actual_c_values[3][3] = {
-        {matmul->C[0][0], matmul->C[0][1], matmul->C[0][2]},
-        {matmul->C[1][0], matmul->C[1][1], matmul->C[1][2]},
-        {matmul->C[2][0], matmul->C[2][1], matmul->C[2][2]}};
-
-    print_matrix("Actual C", actual_c_values);
-
-    bool is_correct = true;
-    for (int i = 0; i < 3; i++)
+    for (int k = 0; k < 3; k++)
     {
-        for (int j = 0; j < 3; j++)
+        b_values[0][0] += k;
+        // Copy matrices to the matmul structure
+        for (int i = 0; i < 3; i++)
         {
-            if (actual_c_values[i][j] != expected_c_values[i][j])
+            for (int j = 0; j < 3; j++)
             {
-                is_correct = false;
+                matmul->A[i][j] = a_values[i][j];
+                matmul->B[i][j] = b_values[i][j];
+            }
+        }
+
+        // Start the multiplication
+        // Depending on your architecture, you might need to set an 'Enable' flag.
+        // Here, I'll assume the multiplication starts automatically when A and B are set.
+
+        // After this, the result should be available in matmul->C
+        // And you can further process or verify it as needed.
+        // Example:
+        uint8_t actual_c_values[3][3] = {
+            {matmul->C[0][0], matmul->C[0][1], matmul->C[0][2]},
+            {matmul->C[1][0], matmul->C[1][1], matmul->C[1][2]},
+            {matmul->C[2][0], matmul->C[2][1], matmul->C[2][2]}};
+
+        print_matrix("Actual C", actual_c_values);
+
+        bool is_correct = true;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (actual_c_values[i][j] != expected_c_values[i][j])
+                {
+                    is_correct = false;
+                    break;
+                }
+            }
+            if (!is_correct)
+            {
                 break;
             }
         }
-        if (!is_correct)
+
+        if (is_correct)
         {
-            break;
+            // Matrix multiplication result is correct!
+            puts("Matrix multiplication result is correct!\n");
+        }
+        else
+        {
+            puts("Matrix multiplication result is incorrect!\n");
         }
     }
-
-    if (is_correct)
-    {
-        // Matrix multiplication result is correct!
-        puts("Matrix multiplication result is correct!\n");
-    }
-    else
-    {
-        puts("Matrix multiplication result is incorrect!\n");
-    }
-
     return 0;
 }
