7e22ca58a5b1054f6fd8f809739c714b19d81970
y2k <git@y2kbugger.com>
2023-09-04 21:12:28 -0400

    Implement 9x9 block based multiplication

--- kos/src/verilation.c
+++ kos/src/verilation.c
@@ -11,6 +11,22 @@ typedef struct
 } MatMul;
 volatile MatMul *const matmul = (MatMul *)0x60003800;
 
+void hardware_matrix_multiply_3(uint8_t a[3][3], uint8_t b[3][3], uint8_t c[3][3])
+{
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+        {
+            matmul->A[i][j] = a[i][j];
+            matmul->B[i][j] = b[i][j];
+        }
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            c[i][j] = matmul->C[i][j];
+}
+
+void hardware_matrix_multiply_3(uint8_t a[3][3], uint8_t b[3][3], uint8_t c[3][3]);
+
 void print_matrix(char label[], uint8_t matrix[3][3])
 {
     putc('\n');
@@ -20,6 +36,23 @@ void print_matrix(char label[], uint8_t matrix[3][3])
     {
         puts("| ");
         for (int j = 0; j < 3; j++)
+        {
+            print_hex(matrix[i][j], 1);
+            putc(' ');
+        }
+        puts("|\n");
+    }
+}
+
+void print_matrix_9(char label[], uint8_t matrix[9][9])
+{
+    putc('\n');
+    puts(label);
+    putc('\n');
+    for (int i = 0; i < 9; i++)
+    {
+        puts("| ");
+        for (int j = 0; j < 9; j++)
         {
             print_hex(matrix[i][j], 1);
             putc(' ');
@@ -53,14 +86,31 @@ void software_matrix_multiply(uint8_t a[3][3], uint8_t b[3][3], uint8_t c[3][3])
     // You can use this to verify that the hardware is working correctly
     for (int i = 0; i < 3; i++)
     {
-        uint8_t row[3] = {a[i][0], a[i][1], a[i][2]};
         for (int j = 0; j < 3; j++)
         {
-            uint8_t col[3] = {b[0][j], b[1][j], b[2][j]};
             uint8_t sum = 0;
             for (int k = 0; k < 3; k++)
             {
-                sum += row[k] * col[k];
+                sum += a[i][k] * b[k][j];
+            }
+            c[i][j] = sum;
+        }
+    }
+}
+
+void software_matrix_multiply_9(uint8_t a[9][9], uint8_t b[9][9], uint8_t c[9][9])
+{
+    // Manually multiply the matrices in software
+    // This is the same algorithm as in the verilated hardware
+    // You can use this to verify that the hardware is working correctly
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            uint8_t sum = 0;
+            for (int k = 0; k < 9; k++)
+            {
+                sum += a[i][k] * b[k][j];
             }
             c[i][j] = sum;
         }
@@ -73,25 +123,8 @@ void check_matrix_multiply(uint8_t a[3][3], uint8_t b[3][3])
     software_matrix_multiply(a, b, expected_c);
     print_matrix("Expected C", expected_c);
 
-    // Copy matrices to the MatMul hardware
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            matmul->A[i][j] = a[i][j];
-            matmul->B[i][j] = b[i][j];
-        }
-    }
-
-    // Read the result from the MatMul hardware
     uint8_t actual_c_values[3][3];
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            actual_c_values[i][j] = matmul->C[i][j];
-        }
-    }
+    hardware_matrix_multiply_3(a, b, actual_c_values);
     print_matrix("Actual C", actual_c_values);
 
     bool is_correct = true;
@@ -114,6 +147,73 @@ void check_matrix_multiply(uint8_t a[3][3], uint8_t b[3][3])
     if (is_correct)
     {
         // Matrix multiplication result is correct!
+        puts("Matrix multiplication result is correct!\n");
+    }
+    else
+    {
+        puts("Matrix multiplication result is incorrect!\n");
+    }
+}
+
+void check_matrix_multiply_9(uint8_t a[9][9], uint8_t b[9][9])
+{
+    uint8_t expected_c[9][9] = {};
+    software_matrix_multiply_9(a, b, expected_c);
+    print_matrix_9("Expected C", expected_c);
+
+    uint8_t actual_c_values[9][9] = {};
+
+    for (int block_row = 0; block_row < 3; block_row++)
+    {
+        for (int block_col = 0; block_col < 3; block_col++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                uint8_t subA[3][3], subB[3][3], subC[3][3] = {};
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        subA[i][j] = a[3 * block_row + i][3 * k + j]; // Modified to use k
+                        subB[i][j] = b[3 * k + i][3 * block_col + j]; // Modified to use k and block_col
+                    }
+                }
+
+                hardware_matrix_multiply_3(subA, subB, subC);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        actual_c_values[3 * block_row + i][3 * block_col + j] += subC[i][j];
+                    }
+                }
+            }
+        }
+    }
+
+    print_matrix_9("Actual C", actual_c_values);
+
+    bool is_correct = true;
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            if (actual_c_values[i][j] != expected_c[i][j])
+            {
+                is_correct = false;
+                break;
+            }
+        }
+        if (!is_correct)
+        {
+            break;
+        }
+    }
+
+    if (is_correct)
+    {
         puts("Matrix multiplication result is correct!\n");
     }
     else
--- kos/src/verilation.h
+++ kos/src/verilation.h
@@ -1,4 +1,6 @@
 #include <stdint.h>
 void print_matrix(char label[], uint8_t matrix[3][3]);
+void print_matrix_9(char label[], uint8_t matrix[9][9]);
 void read_matrix(char label[], uint8_t matrix[3][3]);
 void check_matrix_multiply(uint8_t a[3][3], uint8_t b[3][3]);
+void check_matrix_multiply_9(uint8_t a[9][9], uint8_t b[9][9]);
